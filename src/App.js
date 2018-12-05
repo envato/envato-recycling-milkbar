31bc4626c967353c3da7b5fbaf9c004bad8c9535
Ivor Padilla <ivorjpc@gmail.com>
2018-12-05 10:55:15 -0400

    finished milkbar section

--- src/App.js
+++ src/App.js
@@ -1,73 +1,122 @@
 import React, { Component } from "react";
 import axios from "axios";
 import moment from "moment";
-import CardGrid from './components/CardGrid'
-import logo from './logo.svg'
+import CardGrid from "./components/CardGrid";
+import logo from "./logo.svg";
 import "./App.css";
 
-console.log(logo);
-
 const currentMonth = moment().format("MMMM");
-const previousMonth = moment().subtract(1, "months").format("MMMM");
+const previousMonth = moment()
+  .subtract(1, "months")
+  .format("MMMM");
+const BASE_URL = `https://sheets.googleapis.com/v4/spreadsheets`;
+const API = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
 
 class App extends Component {
-
   state = {
     currentRecyclingData: [],
     previousRecyclingData: [],
+    charityData: [],
     isLoading: true
-  }
+  };
 
   componentDidMount = async () => {
-    const BASE_URL = `https://sheets.googleapis.com/v4/spreadsheets`;
-    const API = await axios.create({
-      baseURL: BASE_URL,
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-
-    const { data: { values: currentRecyclingData }} = await API.get(
-      `/1sNk9S1m2_kJmdj6FwesztvvdrzKFRRkBn7JFQGNY8TQ/values/recycling:${currentMonth.toLowerCase()}!A5:D26?key=AIzaSyDvK1O8LuQKbBH8UBePNCib-vtNmiIbqs0`
+    const {
+      data: { values: currentRecyclingData }
+    } = await API.get(
+      `/${
+        process.env.REACT_APP_SHEET_ID
+      }/values/recycling:${currentMonth.toLowerCase()}!A5:D?key=${
+        process.env.REACT_APP_API_KEY
+      }`
     );
-    const { data: { values: previousRecyclingData } } = await API.get(`/1sNk9S1m2_kJmdj6FwesztvvdrzKFRRkBn7JFQGNY8TQ/values/recycling:${previousMonth.toLowerCase()}!B5:C25?key=AIzaSyDvK1O8LuQKbBH8UBePNCib-vtNmiIbqs0`);
+    const {
+      data: { values: previousRecyclingData }
+    } = await API.get(
+      `/${
+        process.env.REACT_APP_SHEET_ID
+      }/values/recycling:${previousMonth.toLowerCase()}!B5:C?key=${
+        process.env.REACT_APP_API_KEY
+      }`
+    );
+    const {
+      data: { valueRanges: currentMonthCharityData }
+    } = await API.get(
+      `/${
+        process.env.REACT_APP_SHEET_ID
+      }/values:batchGet?ranges=milkbar:${currentMonth.toLowerCase()}!B3&ranges=milkbar:${currentMonth.toLowerCase()}!B4&ranges=milkbar:${currentMonth.toLowerCase()}!B7:B&key=${
+        process.env.REACT_APP_API_KEY
+      }`
+    );
+
+    let charityData = [];
+
+    currentMonthCharityData.forEach(function(v) {
+      const { values } = v;
+
+      values.length === 1
+        ? charityData.push(values[0][0])
+        : charityData.push(
+            values.reduce((acc, v) => {
+              acc.push(v[0]);
+              return acc;
+            }, [])
+          );
+    });
 
     this.setState({
       currentRecyclingData,
       previousRecyclingData,
+      charityData: {
+        name: charityData[0],
+        summary: charityData[1],
+        amountRaised: charityData[2]
+      },
       isLoading: false
     });
-    
   };
 
   render() {
-
-    const { currentRecyclingData, isLoading, previousRecyclingData } = this.state;
+    const {
+      currentRecyclingData,
+      isLoading,
+      previousRecyclingData,
+      charityData
+    } = this.state;
 
     const today = moment(new Date()).format("DD/MM");
-    
+
     const currentDayData = currentRecyclingData.filter(v => {
       let [date] = v;
 
       return date === today;
     });
 
-    const todayIndex = currentRecyclingData.findIndex( v => {
+    const todayIndex = currentRecyclingData.findIndex(v => {
       return v === currentDayData[0];
-    })
+    });
 
-    const [previousDayData] = currentRecyclingData.slice(todayIndex - 1, todayIndex)
-
+    const [previousDayData] = currentRecyclingData.slice(
+      todayIndex - 1,
+      todayIndex
+    );
+    
     return (
       <div className="App">
         <CardGrid
           currentRecyclingData={currentRecyclingData}
           previousRecyclingData={previousRecyclingData}
-          currentDayData={currentDayData} 
+          currentDayData={currentDayData}
           previousDayData={previousDayData}
-          isLoading={isLoading}  
+          isLoading={isLoading}
+          charityData={charityData}
         />
-        <img className="logo" src={logo} width="200" alt=""/>
+        <img className="logo" src={logo} width="200" alt="" />
       </div>
     );
   }
--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -2,9 +2,8 @@ import React from "react";
 import Card from "./Card";
 import moment from "moment";
 
-
-const calculateTotalAverage = (arr) => {
- return arr
+const calculateTotalAverage = arr => {
+  return arr
     .reduce((acc, v) => {
       acc.push((v[1] / v[0]) * 100);
       return acc;
@@ -12,10 +11,9 @@ const calculateTotalAverage = (arr) => {
     .reduce((acc, v) => {
       return acc + v / arr.length;
     }, 0);
-}
+};
 
 function CardGrid(props) {
-
   let date, prevDaynonContaminated, prevDaycontaminated;
   if (props.previousDayData) {
     [date, prevDaynonContaminated, prevDaycontaminated] = props.previousDayData;
@@ -31,23 +29,23 @@ function CardGrid(props) {
   const currengAvgTotal = calculateTotalAverage(monthRecyclingData);
   const prevAvgTotal = calculateTotalAverage(props.previousRecyclingData);
 
-  const [total, monthContaminated] = monthRecyclingData.reduce(function (r, a) {
-    a.forEach(function (b, i) {
+  const [total, monthContaminated] = monthRecyclingData.reduce(function(r, a) {
+    a.forEach(function(b, i) {
       r[i] = (r[i] || 0) + b;
     });
     return r;
   }, []);
 
+  const dateFormatted = moment(date, "DD/MM").format("ddd, D MMM");
+  const monthName = moment(new Date()).format("MMMM");
+  const amountRaised = props.charityData.amountRaised && props.charityData.amountRaised.reduce((acc, v) => acc + parseFloat(v, 10), 0);
   return <div className="grid">
       <Card>
         {!props.isLoading ? <>
             <h1>Recycling</h1>
             <ul className="box__content">
               <li>
-                {date && <h3>
-                    {" "}
-                    {moment(date, "DD/MM").format("ddd, D MMM")}{" "}
-                  </h3>}
+                {date && <h3>{dateFormatted}</h3>}
                 <p>
                   {" "}
                   {`${prevDaycontaminated} of ${prevDaynonContaminated} bags contaminated (${Math.round(
@@ -57,46 +55,47 @@ function CardGrid(props) {
               </li>
               <li>
                 <h3>This month so far</h3>
-            <p>{`${monthContaminated} of ${total} bags contaminated (${Math.round(
-              (monthContaminated / total) * 100
-            )}%)`}</p>
+                <p>{`${monthContaminated} of ${total} bags contaminated (${Math.round((monthContaminated / total) * 100)}%)`}</p>
               </li>
               <li>
                 <h3>Last month</h3>
-             <p>{`${Math.round(prevAvgTotal)}%`} contamination rate</p>
+                <p>{`${Math.round(prevAvgTotal)}%`} contamination rate</p>
               </li>
             </ul>
 
             <div className="box__results">
-              
-          <h2>On track to improving? {currengAvgTotal < prevAvgTotal ? <span role="img" aria-label="thumbs up">👍</span> : <span role="img" aria-label="thumbs down">👎</span> }</h2>
-
+              <h2>
+                On track to improving? {currengAvgTotal < prevAvgTotal ? <span role="img" aria-label="thumbs up">
+                    👍
+                  </span> : <span role="img" aria-label="thumbs down">
+                    👎
+                  </span>}
+              </h2>
             </div>
           </> : "loading..."}
       </Card>
-      <div className="box">
-        <h1>Milk bar charity</h1>
+      <Card>
+        {!props.isLoading ? <>
+            <h1>Milk bar charity</h1>
 
-        <ul className="box__content">
-          <li>
-            <h3>December Charity</h3>
-            <p>Some charity name</p>
-          </li>
-          <li>
-            <h3>About</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-              hic nulla impedit recusandae repellendus veniam voluptatem
-              provident consectetur vitae natus, nihil accusamus possimus
-              corporis tenetur? Nostrum veritatis ipsum qui accusantium.
-            </p>
-          </li>
-        </ul>
+            <ul className="box__content">
+              <li>
+                <h3>{monthName} Charity</h3>
+                <p>{props.charityData.name}</p>
+              </li>
+              <li>
+                <h3>About</h3>
+                <p>{props.charityData.summary}</p>
+              </li>
+            </ul>
 
-        <div className="box__results">
-          Amount raised so far: <span>$527</span>
-        </div>
-      </div>
+            <div className="box__results">
+              <h2>
+                Amount raised so far: <span>${amountRaised}</span>
+              </h2>
+            </div>
+          </> : "loading..."}
+      </Card>
     </div>;
 }
 
